f9d7e1d0b9904c9a3ebcac644d397084f6e1ab75
kumudinidesale <kdesale1211@gmail.com>
2022-03-29 13:21:40 +0530

    added:Ability To Creat Contact

---
+++ src/com/java/AddressBook.java
@@ -0,0 +1,8 @@
+package com.java;
+
+public class AddressBook {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+}
---
+++ src/com/java/AddressBookMain.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.List;
+
+public class AddressBookMain {
+	private List<Contact> contacts;
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+}
